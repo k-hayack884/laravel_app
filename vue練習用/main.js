81752efb43f8441be044fee0202029849d7b08e3
k-hayack884 <hayack885@gmail.com>
2021-12-13 16:00:24 +0900

    aa

--- vue練習用/main.js
+++ vue練習用/main.js
@@ -1,10 +1,15 @@
 const app=Vue.createApp({
   data:()=>({
-    
+    newItem:'',
+    todos:[]
   }),
   methods:{
     books:function(){
       console.log('ﾌﾞｯｸｽ!')
+      let todo={
+        item:this.newItem
+      }
+      this.todos.push(todo)
     }
   }
 })
