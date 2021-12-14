9e2cfeee1a67ae327fdfacd66f020eb0ccd41fc1
k-hayack884 <hayack885@gmail.com>
2021-12-14 11:16:50 +0900

    動作確認

--- vue練習用/main.js
+++ vue練習用/main.js
@@ -5,11 +5,20 @@ const app=Vue.createApp({
   }),
   methods:{
     books:function(){
-      console.log('ﾌﾞｯｸｽ!')
+      //console.log('ﾌﾞｯｸｽ!')
+      if(this.newItem==='')return
+
       let todo={
-        item:this.newItem
+        item:this.newItem,
+        isDone: false
       }
       this.todos.push(todo)
+      this.newItem=''
+    },
+    sarugasso:function(index){
+      //console.log('姑息な手を')
+      //console.log(index)
+      this.todos.splice(index,1)
     }
   }
 })
