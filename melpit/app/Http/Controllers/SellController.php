a99a41117a2036b59ce5f640d0e348115d2bb727
k-hayack884 <hayack885@gmail.com>
2021-12-15 14:34:30 +0900

    出品処理途中

--- melpit/app/Http/Controllers/SellController.php
+++ melpit/app/Http/Controllers/SellController.php
@@ -3,17 +3,45 @@
 namespace App\Http\Controllers;
 use App\Models\ItemCondition;
 use App\Models\PrimaryCategory;
+use App\Http\Requests\SellRequest;
+use app\Models\Item;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SellController extends Controller
 {
     public function showSellForm()
-    {   $categories=PrimaryCategory::orderBy('sort_no')->get();
+    {   //$categories=PrimaryCategory::orderBy('sort_no')->get();
+        $categories=PrimaryCategory::query()
+        ->with([
+            'secondaryCategories'=>function($query){
+                $query->orderBy('sort_no');
+            }
+        ])
+        ->orderBy('sort_no')
+        ->get();
         $conditions=ItemCondition::orderBy('sort_no')->get();
 
         return view('sell')
         ->with('categories',$categories)
         ->with('conditions',$conditions);
+
+    }
+    public function sellItem(SellRequest $request){
+        $user=Auth::user();
+
+        $item=new Item();
+        $item->sell_id=$user->id;
+        $item->name=$request->input('name');
+        $item->description=$request->input('description');
+        $item->secondary_categary_id=$request->input('category');
+        $item->item_condition_id=$request->input('condition');
+        $item->price=$request->input('price');
+        $item->state=Item::STATE_SELLING;
+        $item->save();
+
+        return redirect()->back()->with('status','商品を出品しました');
+
 
     }
 }
