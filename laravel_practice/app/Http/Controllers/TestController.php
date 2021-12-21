4421dc28763c1d857f970305dea1763d50ca8ed0
k-hayack884 <hayack885@gmail.com>
2021-12-21 11:59:43 +0900

    12/21午前

---
+++ laravel_practice/app/Http/Controllers/TestController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Test;
+class TestController extends Controller
+{
+    public function index(){
+        $values=Tests::all();
+        //dd($values);
+        return view('tests.test',compact('values'));
+    }
+}
