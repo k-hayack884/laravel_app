207f2bf56b1a6ee061e3e563ad632181fcf30f01
k-hayack884 <hayack885@gmail.com>
2021-12-27 13:18:54 +0900

    スロット回すお

--- laravel_ec/resources/views/tests/component-test1.blade.php
+++ laravel_ec/resources/views/tests/component-test1.blade.php
@@ -1,1 +1,4 @@
-ドルベ！
+<x-tests.app>
+    <x-slot name="header">ヘッダー１</x-slot>
+ドルベ!
+</x-tests.app>
--- laravel_ec/resources/views/tests/component-test2.blade.php
+++ laravel_ec/resources/views/tests/component-test2.blade.php
@@ -1,1 +1,3 @@
-ナッシュ！
+<x-tests.app>
+    <x-slot name="header">メラグ・・・</x-slot>
+    ナッシュ!</x-tests.app>
