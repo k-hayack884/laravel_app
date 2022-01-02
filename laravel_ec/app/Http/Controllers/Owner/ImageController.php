7123ca8b3351f78cc9a1a8014c3ec10e8c7e47ea
k-hayack884 <hayack885@gmail.com>
2022-01-02 14:59:28 +0900

    n+1の解決

--- laravel_ec/app/Http/Controllers/Owner/ImageController.php
+++ laravel_ec/app/Http/Controllers/Owner/ImageController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\UploadImageRequest;
 use Illuminate\Support\Facades\Auth;
-use App\Models\image;
+use App\Models\Image;
 use App\Services\ImageService;
 use Illuminate\Support\Facades\Storage;
 
--- laravel_ec/app/Http/Controllers/Owner/ProductController.php
+++ laravel_ec/app/Http/Controllers/Owner/ProductController.php
@@ -4,8 +4,42 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Image;
+use App\Models\Product;
+use App\Models\SecondaryCategory;
+use App\Models\Owner;
 
 class ProductController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+        $this->middleware(function ($request, $next) {
+
+            $id = $request->route()->parameter('product'); //直接入力した数字
+            if (!is_null($id)) {
+                $productsOwnerId = Product::findOrFail($id)->shop->owner->id;
+                $productId = (int)$productsOwnerId;
+                if ($productId !== Auth::id()) {
+                    abort(404);
+                }
+            }
+            return $next($request);
+        });
+    }
+
+    public function index()
+    {
+        // $products=Owner::findOrFail(Auth::id())->shop->product;
+        $ownerInfo=Owner::with('shop.product.imageFirst')
+        ->where('id',Auth::id())->get();
+        // dd($OwnerInfo);
+        // foreach($ownerInfo as $owner){
+        //     foreach($owner->shop->product as $product){
+        //         $product->imageFirst->filename;
+        //     }
+        // }
+        return view('owner.products.index', compact('ownerInfo'));
+    }
 }
